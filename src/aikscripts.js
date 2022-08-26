5abb2ab8eec46dfdc58405e6b124e98ff7633e0f
Aik <ikogweassurance85@gmail.com>
2022-08-26 07:37:38 +0100

    I am still styling for mobile

---
+++ src/aikscripts.js
@@ -0,0 +1,5 @@
+const imageProduct = document.getElementById("image_product")
+//swap images
+if (!window.innerWidth <= 400){
+    imageProduct.setAttribute("src", "././images/image-product-desktop.jpg")
+}
