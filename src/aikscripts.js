2bf2587c72274d46c3bad8e8616d1d4217d16c7d
Aik <ikogweassurance85@gmail.com>
2022-08-26 08:14:02 +0100

    I started the styling for desktop

--- src/aikscripts.js
+++ src/aikscripts.js
@@ -1,5 +1,5 @@
 const imageProduct = document.getElementById("image_product")
 //swap images
 if (!window.innerWidth <= 400){
-    imageProduct.setAttribute("src", "././images/image-product-desktop.jpg")
+    imageProduct.style.backgroundImage= 'url("../images/image-product-desktop.jpg")';
 }
