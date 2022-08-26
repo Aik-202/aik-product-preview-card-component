f6fd27207f73c8d974e5a08f59b821f96eb71951
Aik <ikogweassurance85@gmail.com>
2022-08-26 13:41:24 +0100

    I deleted the src folder

--- src/aikscripts.js
+++
@@ -1,5 +0,0 @@
-const imageProduct = document.getElementById("image_product")
-//swap images
-if (!window.innerWidth <= 400){
-    imageProduct.style.backgroundImage= 'url("../images/image-product-desktop.jpg")';
-}
